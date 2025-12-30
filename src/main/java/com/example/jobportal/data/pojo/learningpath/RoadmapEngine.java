ce827ce5acbc5d5e4af452b96327830eaf0eaa79
Dinh Dat <tddat19102004@gmail.com>
2025-12-30 22:41:48 +0700

    d

--- src/main/java/com/example/jobportal/data/pojo/learningpath/RoadmapEngine.java
+++
@@ -1,9 +0,0 @@
-package com.example.jobportal.data.pojo.learningpath;
-
-import com.example.jobportal.data.entity.JobSkill;
-
-import java.util.List;
-
-public interface RoadmapEngine {
-    List<LearningPathStep> buildForSkill(String skillName, JobSkill required);
-}
